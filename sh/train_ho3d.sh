3e0cacd2f9501faff862142a64bca5abb026142c
lzfff12 <55379485+lzfff12@users.noreply.github.com>
2023-07-28 16:38:03 +0800

    Update train_ho3d.sh

--- sh/train_ho3d.sh
+++ sh/train_ho3d.sh
@@ -1,6 +1,6 @@
         nohup\
         python traineval.py --HO3D_root /data1/zhifeng/ho3dv2 \
-        --host_folder  /data1/zhifeng/cvpr/host_folder/ho3d_my_model_one \
+        --host_folder  host_folder/ho3d_my_model \
         --dex_ycb_root /data1/zhifeng/dex-ycb \
         --epochs 70 \
         --inp_res 256 \
@@ -12,7 +12,7 @@
         --lr_decay_step 10 \
         --test_batch 64 \
         --use_ho3d \
-        > train_check_ho3d_one.log 2>&1 &
+        > train_check_ho3d.log 2>&1 &
 
         #        CUDA_VISIBLE_DEVICES=0,1,3,4\
         
