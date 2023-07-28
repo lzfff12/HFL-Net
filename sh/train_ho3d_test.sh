a43f3249f555636c6c0ac651c7c2df0118d1d389
lzfff12 <55379485+lzfff12@users.noreply.github.com>
2023-07-28 16:39:48 +0800

    Update train_ho3d_test.sh

--- sh/train_ho3d_test.sh
+++ sh/train_ho3d_test.sh
@@ -1,6 +1,6 @@
         nohup\
         python traineval.py --HO3D_root /data1/zhifeng/ho3dv2 \
-        --host_folder  host_folder/dex-ycb \
+        --host_folder  host_folder/ho3d \
         --dex_ycb_root /data1/zhifeng/dex-ycb \
         --epochs 70 \
         --inp_res 256 \
